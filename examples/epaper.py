1786d6288430ff61f38c227535476a14ee08fe59
Hugo Tausch <hugoskummerkasten@gmail.com>
2024-01-09 21:41:55 +0100

    added tickets

--- examples/epaper.py
+++ examples/epaper.py
@@ -6,6 +6,22 @@ import sys
 import os
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import locale
+
+locale.setlocale(locale.LC_TIME, 'de_DE')
+
+picdir = os.path.join(os.path.dirname(
+    os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(
+    os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+font22 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 22)
+fontIcon = ImageFont.truetype(os.path.join(
+    picdir, 'materialdesignicons-webfont.ttf'), 22)
 
 
 class word_press:
@@ -13,15 +29,21 @@ class word_press:
         api_url = 'https://cvjm-walheim.de/wp-json/tribe/events/v1/events?_embed&page=1'
         response = requests.get(api_url)
         response_json = response.json()
-        logging.info("Events reseved")
+        logging.info("got events")
+        return response_json
+
+    def read_wordpress_tickets():
+        api_url = 'https://cvjm-walheim.de/wp-json/wp/v2/tribe_rsvp_tickets?page=1'
+        response = requests.get(api_url)
+        response_json = response.json()
+        logging.info("got tickets")
         return response_json
 
     def create_images():
         edge = 10
         space = 50
         lineHeight = 27
-        font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-        font22 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 22)
+        lineGap = 15
         logging.info("ePaper Example")
         # Clean image
         logging.info("Get data from wordpress")
@@ -35,15 +57,32 @@ class word_press:
         draw_red = ImageDraw.Draw(red)
 
         events = word_press.read_wordpress_events()
+        tickets = word_press.read_wordpress_tickets()
         line = edge
         for event in events['events']:
             soup = BeautifulSoup(event['title'], 'html.parser')
-            draw_red.text((edge, line),
-                          event['utc_start_date_details']['day']+"."+event['utc_start_date_details']['month'], font=font22, fill=(255, 0, 0, 255))
+            startTime = datetime.strptime(
+                event['start_date'], '%Y-%m-%d %H:%M:%S')
+
+            draw_red.text((edge + epd.width/2, line),
+                          startTime.strftime('%A %d %B %Y'), font=font22, fill=(255, 0, 0, 255))
+            # icon = chr(0xf207)
+            # draw_red.text((edge, line),
+            #               icon, font=fontIcon, fill=(255, 0, 0, 255))
             line += lineHeight
-            draw_black.text((edge, line),
+            draw_black.text((edge + epd.width/2, line),
                             soup.text, font=font22, fill=(0, 0, 0, 255))
+            line += lineHeight + lineGap
+        line = edge
+        for ticket in tickets:
+
+            draw_black.text((edge, line),
+                            str(ticket['title']['rendered']), font=font22, fill=(0, 0, 0, 255))
+
             line += lineHeight
+            draw_red.text((edge, line),
+                          str(ticket['meta']['_stock'])+'/'+str(ticket['meta']['_tribe_ticket_capacity']), font=font22, fill=(255, 0, 0, 255))
+            line += lineHeight + lineGap
 
         combined = Image.alpha_composite(combined, black)
         combined = Image.alpha_composite(combined, red)
@@ -53,14 +92,6 @@ class word_press:
 class epd:
     width = 800
     height = 480
-
-
-picdir = os.path.join(os.path.dirname(
-    os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(
-    os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
 
 
 logging.basicConfig(level=logging.DEBUG)
